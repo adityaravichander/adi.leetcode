f87a74d9dfa8139a501aba5979fa4fe465d977f5
Aditya Ravichander <aditya.ravichander@gmail.com>
2026-06-27 16:53:01 -0500

    update pseudo code
    Added pseudocode for contains duplicate problem and implemented sorting method.

--- arrays/contains_duplicate/contains_duplicate.py
+++ arrays/contains_duplicate/contains_duplicate.py
@@ -1,4 +1,26 @@
+'''
+ARRAYS _ CONTAINS DUPLICATE
+PSEUDO CODE
+
+A. HASH SET
+1. FOR array
+2. if nums[i] in set() -- True
+3. else add nums[i] to set
+
+B. SORTING
+1. sort
+2. FOR iterate array
+3. if nums[i] = nums[i-1] -- True, else False
+
+C. BRUTE FORCE
+
+1. FOR I  
+2. inside that FOR J = i+1
+3. if nums[i] = nums[j] -- True, else False
+
+'''
 from typing import List
+
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
